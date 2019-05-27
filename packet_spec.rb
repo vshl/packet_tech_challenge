e9f433c74534b353954f1405c7e620b61b3dd6b1
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2019-05-27 16:54:10 -0700

    Bug fixes

--- packet_spec.rb
+++ packet_spec.rb
@@ -17,7 +17,7 @@ describe Device do
 
   describe 'tear_down_device' do
     it 'tears down device' do
-      unless device.my_device.provisioning?
+      unless device&.my_device&.provisioning?
         expect { device.tear_down_device }.to change { device.number_of_devices }.by(-1)
       end
     end
@@ -25,21 +25,25 @@ describe Device do
 
   describe 'boot' do
     it 'does not power up a device when provisioning' do
-      expect(device.boot).to eq false if device.my_device.provisioning?
+      expect(device.boot).to eq false if device&.my_device&.provisioning?
     end
 
     it 'does power up a device' do
-      expect(device.boot).to eq true unless device.my_device.provisioning?
+      unless device.my_device.nil?
+        expect(device.boot).to eq true unless device&.my_device&.provisioning?
+      end
     end
   end
 
   describe 'powerdown' do
     it 'does not power down a device when provisioning' do
-      expect(device.powerdown).to eq false if device.my_device.provisioning?
+      expect(device.powerdown).to eq false if device&.my_device&.provisioning?
     end
 
     it 'does power down a device' do
-      expect(device.powerdown).to eq true unless device.my_device.provisioning?
+      unless device.my_device.nil?
+        expect(device.powerdown).to eq true unless device&.my_device&.provisioning?
+      end
     end
   end
 end
