bb71820c8a111a90b4cde1f5e2751565d086ae13
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2019-05-27 14:52:48 -0700

    Device class to create and tear down device

---
+++ packet.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require 'packethost'
+
+class Device
+  PROJECT_ID = 'ca73364c-6023-4935-9137-2132e73c20b4'
+
+  def initialize
+    @client = Packet::Client.new('NLxG4o3xNjyKyddT7g9WX1rHyPYfHeBZ')
+  end
+
+  def create_new_device
+    return unless my_device.nil?
+
+    client.create_device(new_device)
+  end
+
+  def tear_down_device
+    return if my_device.nil? || my_device.provisioning?
+
+    client.delete_device(my_device)
+  rescue Packet::Error => e
+    puts e
+  end
+
+  def new_device
+    @new_device ||=
+      Packet::Device.new(
+        project_id: PROJECT_ID,
+        hostname: 'vishaltest.packethost.net',
+        operating_system: os.to_hash,
+        facility: facility.to_hash,
+        plan: plan.to_hash
+      )
+  rescue Packet::Error => e
+    puts e
+  end
+
+  def boot
+    return if my_device.nil? || my_device.active?
+
+    client.power_on_device(my_device)
+  end
+
+  def powerdown
+    return if my_device.nil? || my_device.inactive?
+
+    client.power_off_device(my_device)
+  end
+
+  def number_of_devices
+    client.list_devices(PROJECT_ID).count
+  end
+
+  def my_device
+    client.list_devices(PROJECT_ID).find { |device| device.hostname == 'vishaltest.packethost.net' }
+  end
+
+  attr_reader :client
+
+  private
+
+  def os
+    @os ||= client.list_operating_systems.find { |os| os.slug == 'freebsd_10_3' }
+  end
+
+  def facility
+    @facility ||= client.list_facilities.find { |facility| facility.code == 'ewr1' }
+  end
+
+  def plan
+    @plan ||= client.list_devices(PROJECT_ID)[0].plan
+  end
+end
---
+++ packet_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative 'packet'
+
+describe Device do
+  device = Device.new
+
+  describe 'create_new_device' do
+    it 'creates new device' do
+      if device.my_device.nil?
+        expect { device.create_new_device }.to change { device.number_of_devices }.by(1)
+      else
+        expect { device.create_new_device }.to change { device.number_of_devices }.by(0)
+      end
+    end
+  end
+
+  describe 'tear_down_device' do
+    it 'tears down device' do
+      if device.my_device.provisioning?
+        expect { device.tear_down_device }.to change { device.number_of_devices }.by(0)
+      else
+        expect { device.tear_down_device }.to change { device.number_of_devices }.by(-1)
+      end
+    end
+  end
+end
